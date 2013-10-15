ec9aba4cea0992f72611a2558d60837f9291b728
Johan Blakkisrud <blakkisrud@gmail.com>
2013-10-15 20:13:16 +0200

    More bug-fix

--- RUN_WAI.sh
+++ RUN_WAI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting up"
-ifconfig > tt1.txt
-scp tt1.txt blakkisr@lynx.stud.ntnu.no:public_html
+ifconfig > ~/kawadapi.txt
+scp ~/kawadapi.txt blakkisr@lynx.stud.ntnu.no:public_html
 echo "Done"
