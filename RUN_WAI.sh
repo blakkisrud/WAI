677ee8888785dea230f9a7e270ad2d5b09de996d
Johan Blakkisrud <blakkisrud@gmail.com>
2013-10-22 02:40:32 +0200

    Add comments - Should now work properly

--- RUN_WAI.sh
+++ RUN_WAI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting up"
-ifconfig > ~/kawadapi.txt
-scp ~/kawadapi.txt blakkisr@lynx.stud.ntnu.no:public_html
+ifconfig > ~/kawadapi.txt # Storing relevant information in home-directory
+scp ~/kawadapi.txt blakkisr@lynx.stud.ntnu.no:public_html #Given that a place on the web has been set up with ssh-keys
 echo "Done"
